3d59d5bda77faedd7b6d01eb7273d30ea27def56
LucasGimenezKramer <159184152+LucasGimenezKramer@users.noreply.github.com>
2025-12-16 23:13:39 -0300

    Add apple touch icon to layout
    Added apple touch icon link to the head section.

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+	<head>
+  		<link rel="apple-touch-icon" href="/lux-logo.png" />
+	</head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
